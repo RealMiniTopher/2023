1c5bebfd939b1e7138a63b03775e9c100529bf23
Christian Mora <114608984+RealMiniTopher@users.noreply.github.com>
2022-12-06 10:35:41 -0500

    Update TEST3.py

--- ATEST/TEST3.py
+++ ATEST/TEST3.py
@@ -1,5 +1,7 @@
 print("Hello there! I am a repeating bot. I will repeat whatever you want me to! Cool right? Just type what you want me to say and I will say it. Have fun and please don't make me say anything stupid.")
 repeat=input()
+print("-")
+print("-")
 import sys,time
 def sprint(str):
    for c in str + '\n':
