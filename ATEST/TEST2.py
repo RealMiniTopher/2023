9e834651129b686a4b466229e979729fc9ca5d70
Christian Mora <114608984+RealMiniTopher@users.noreply.github.com>
2022-12-06 10:33:27 -0500

    Create TEST2.py

---
+++ ATEST/TEST2.py
@@ -0,0 +1,10 @@
+print("Hello there! I am a repeating bot. I will repeat whatever you want me to! Cool right? Just type what you want me to say and I will say it. Have fun and please don't make me say anything stupid.")
+repeat=input()
+import sys,time
+def sprint(str):
+   for c in str + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+     time.sleep(1./5)
+
+sprint(f'{repeat}')
